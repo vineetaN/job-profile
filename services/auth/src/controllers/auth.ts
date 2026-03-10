1bb6de8f586a4f5c21ebc563c80a88b5cbb87570
vineeta <220020057@iitdh.ac.in>
2026-03-10 20:36:32 +0530

    Register User functionality with jwt token created successfully

--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -1,7 +1,10 @@
+import getBuffer from "../utils/buffer.js";
 import { sql } from "../utils/db.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { TryCatch } from "../utils/TryCatch.js";
 import bcrypt from "bcrypt"
+import axios from "axios";
+import jwt from "jsonwebtoken";
 
 
 export const registerUser = TryCatch(async(req , res , next)=>{
@@ -34,10 +37,39 @@ registeredUser = user;
 else if(role === "jobseeker")
 {
   const file = req.file
-  const [user] = await sql `INSERT INTO users (name , email , password , phone_number , role) VALUES
-  (${name} , ${email} , ${hashPassword} , ${phoneNumber} , ${role}) RETURNING user_id , name , email , phone_number , role , created_at
+
+  if(!file)
+  {
+    throw new ErrorHandler(400 , "Resume file is required for jobseekers");
+  }
+
+  const fileBuffer = getBuffer(file);
+  if(!fileBuffer || !fileBuffer.content)
+  {
+    throw new ErrorHandler(500 , "Failed to generate buffer");
+  }
+const {data} = await axios.post(`${process.env.UPLOAD_SERVICE}/api/utils/upload` , {buffer : fileBuffer.content})
+
+  const [user] = await sql `INSERT INTO users (name , email , password , phone_number , role , bio , resume , resume_public_id) VALUES
+  (${name} , ${email} , ${hashPassword} , ${phoneNumber} , ${role} , ${bio} , ${data.url} , ${data.public_id}) RETURNING user_id , name , email , phone_number , role ,bio , resume, created_at
   `;
+
+  registeredUser = user;
 }
-  res.json(email);
+
+const token = jwt.sign({
+  id:registeredUser?.user_id
+} , process.env.JWT_SEC as string,
+
+{
+  expiresIn: "15d",
+})
+
+  res.json({
+    message: "User Registered successfully",
+    registeredUser ,
+    token
+
+  });
 
 })
