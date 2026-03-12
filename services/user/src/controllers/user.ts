a0249118be844290cf44d148973cf7dbe3ef32db
vineeta <220020057@iitdh.ac.in>
2026-03-13 03:05:18 +0530

    Feat : API for updating Resume made successfully

--- services/user/src/controllers/user.ts
+++ services/user/src/controllers/user.ts
@@ -96,3 +96,52 @@ export const updateProfilePic = TryCatch(async(req:AuthenticatedRequest , res) =
   })
 })
 
+
+
+
+
+
+
+
+export const updateResume = TryCatch(async(req:AuthenticatedRequest , res) => {
+  const user = req.user;
+  if(!user)
+  {
+    throw new ErrorHandler(401 , "Authentication required");
+  }
+  const file = req.file;
+  if(!file)
+  {
+    throw new ErrorHandler(400 , "no pdf file to upload");
+  }
+  const oldPublicId = user.resume_public_id
+  const fileBuffer = getBuffer(file)
+
+  if(!fileBuffer || !fileBuffer.content)
+  {
+    throw new ErrorHandler(500 , "failed to generate buffer");
+  }
+  // const {data: uploadResult} = await axios.post(`${process.env.UPLOAD_SERVICE}/api/utils/upload` ,
+  //   { buffer:fileBuffer.content,
+  //     public_id :oldPublicId,
+  //   }
+
+    
+  // );
+  const {data: uploadResult} = await axios.post<{url: string, public_id: string}>(`${process.env.UPLOAD_SERVICE}/api/utils/upload` ,
+  { buffer:fileBuffer.content,
+    public_id :oldPublicId,
+  }
+);
+
+
+  const [updatedUser] = await sql `
+  UPDATE users SET resume = ${uploadResult.url},resume_public_id = ${uploadResult.public_id} WHERE user_id = ${user.user_id} RETURNING user_id , name , resume;
+  `;
+
+  res.json({
+    message:"Resume updated successfully",
+    updatedUser
+  })
+})
+
--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { isAuth } from "../middleware/auth.js";
-import { getUserProfile, myProfile, updateProfilePic, updateUserProfile } from "../controllers/user.js";
+import { getUserProfile, myProfile, updateProfilePic, updateResume, updateUserProfile } from "../controllers/user.js";
 import uploadFile from "../middleware/multer.js";
 import multer from "multer";
 const upload = multer({ storage: multer.memoryStorage() });
@@ -11,5 +11,6 @@ router.get("/me" , isAuth , myProfile)
 router.get("/:userId" , isAuth , getUserProfile)
 router.put("/update/profile" , isAuth , updateUserProfile);
 router.put("/update/pic" , isAuth , upload.single('file') , updateProfilePic);
+router.put("/update/resume" , isAuth , upload.single('file') , updateResume);
 
 export default router;
