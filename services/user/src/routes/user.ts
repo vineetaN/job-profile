e764c0301825a8cebb1177133dba96904e031954
vineeta <220020057@iitdh.ac.in>
2026-03-13 04:29:29 +0530

    Feat : API for adding and deleting skills added successfully

--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { isAuth } from "../middleware/auth.js";
-import { getUserProfile, myProfile, updateProfilePic, updateResume, updateUserProfile } from "../controllers/user.js";
+import { addSkillToUser, deleteSkillFromUser, getUserProfile, myProfile, updateProfilePic, updateResume, updateUserProfile } from "../controllers/user.js";
 import uploadFile from "../middleware/multer.js";
 import multer from "multer";
 const upload = multer({ storage: multer.memoryStorage() });
@@ -12,5 +12,8 @@ router.get("/:userId" , isAuth , getUserProfile)
 router.put("/update/profile" , isAuth , updateUserProfile);
 router.put("/update/pic" , isAuth , upload.single('file') , updateProfilePic);
 router.put("/update/resume" , isAuth , upload.single('file') , updateResume);
+router.post("/skill/add" , isAuth , addSkillToUser);
+router.delete("/skill/delete" , isAuth , deleteSkillFromUser)
+
 
 export default router;
