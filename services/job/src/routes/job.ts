75712c93ba5a4d34a185ce9dbd1b8e1325806b00
vineeta <220020057@iitdh.ac.in>
2026-03-14 03:25:59 +0530

    Feat : API for creating Job created successfully

--- services/job/src/routes/job.ts
+++ services/job/src/routes/job.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import { isAuth } from "../middleware/auth.js";
 import uploadFile from "../middleware/multer.js";
-import { createCompany, deleteCompany } from "../controllers/job.js";
+import { createCompany, createJob, deleteCompany } from "../controllers/job.js";
 
 const router = express.Router()
 
 router.post("/company/new" , isAuth , uploadFile , createCompany)
 router.delete("/company/:companyId" , isAuth , deleteCompany)
+router.post("/new" , isAuth , createJob)
 
 export default router;
