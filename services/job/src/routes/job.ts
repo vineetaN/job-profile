fed3e175d2e863074cde6f1483346e8bf22d09d6
vineeta <220020057@iitdh.ac.in>
2026-03-14 03:05:32 +0530

    Feat : API for deleting company created successfully

--- services/job/src/routes/job.ts
+++ services/job/src/routes/job.ts
@@ -1,10 +1,11 @@
 import express from "express"
 import { isAuth } from "../middleware/auth.js";
 import uploadFile from "../middleware/multer.js";
-import { createCompany } from "../controllers/job.js";
+import { createCompany, deleteCompany } from "../controllers/job.js";
 
 const router = express.Router()
 
 router.post("/company/new" , isAuth , uploadFile , createCompany)
+router.delete("/company/:companyId" , isAuth , deleteCompany)
 
 export default router;
