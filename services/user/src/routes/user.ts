42c43cefc530fde021e0444c2845b0f78bb50388
vineeta <220020057@iitdh.ac.in>
2026-03-15 21:04:17 +0530

    Two API added succesfully : 1 . Applying for job and 2 . getting information for all the jobs applied by the user

--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { isAuth } from "../middleware/auth.js";
-import { addSkillToUser, deleteSkillFromUser, getUserProfile, myProfile, updateProfilePic, updateResume, updateUserProfile } from "../controllers/user.js";
+import { addSkillToUser, applyForJob, deleteSkillFromUser, getAllaplications, getUserProfile, myProfile, updateProfilePic, updateResume, updateUserProfile } from "../controllers/user.js";
 import uploadFile from "../middleware/multer.js";
 import multer from "multer";
 const upload = multer({ storage: multer.memoryStorage() });
@@ -14,6 +14,7 @@ router.put("/update/pic" , isAuth , upload.single('file') , updateProfilePic);
 router.put("/update/resume" , isAuth , upload.single('file') , updateResume);
 router.post("/skill/add" , isAuth , addSkillToUser);
 router.delete("/skill/delete" , isAuth , deleteSkillFromUser)
-
+router.post("/apply/job" , isAuth , applyForJob)
+router.get("/application/all" , isAuth , getAllaplications)
 
 export default router;
