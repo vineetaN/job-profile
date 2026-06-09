c7fbd4069fc0da6805beb84e468a104eda38c4d3
vineeta <220020057@iitdh.ac.in>
2026-06-10 02:14:26 +0530

    Login and register api built succesfully

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import authRoutes from "./routes/auth.js"
 import { connectKafka } from "./producer.js";
+import cors from  "cors"
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors())
 
 connectKafka();
 
--- services/user/src/index.ts
+++ services/user/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
 import userRoutes from "./routes/user.js"
+import cors from 'cors'
 
 
 dotenv.config();
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use("/api/user" , userRoutes)
