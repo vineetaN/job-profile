9fd5025c14708fe8a40df81bd7d58ef03712bbd8
vineeta <220020057@iitdh.ac.in>
2026-07-06 19:45:04 +0530

    Feat : Subscription API created successfully

--- frontend/src/app/account/components/info.tsx
+++ frontend/src/app/account/components/info.tsx
@@ -1,7 +1,7 @@
 import { AccountProps } from '@/type'
 import React, { ChangeEvent, useRef, useState } from 'react'
 import { Card } from '@/components/ui/card'
-import { Briefcase, Camera, Edit, FileText, Link, Mail, NotepadText, Phone, UserIcon } from 'lucide-react'
+import { AlertTriangle, Briefcase, Camera, CheckCircle2, Crown, Edit, FileText, Link, Mail, NotepadText, Phone, RefreshCcw, UserIcon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { useAppData } from '@/context/AppContext'
 import { Dialog, DialogContent, DialogHeader, DialogTrigger ,
@@ -10,6 +10,7 @@ import { Dialog, DialogContent, DialogHeader, DialogTrigger ,
 } from '@/components/ui/dialog'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
+import { useRouter } from 'next/router'
 
 
 const Info : React.FC<AccountProps>= ({user , isYourAccount}) => {
@@ -71,6 +72,9 @@ const changeResume = (e : ChangeEvent<HTMLInputElement>) => {
    updateResume(formData);
   }
 }
+
+
+const router = useRouter()
 
 
   return (
@@ -241,10 +245,112 @@ onChange={changeResume}
   </div>
   }
 
+{/**Subscription section */}
+{
+  isYourAccount && <>
+  {user.role === "jobseeker" && <div className="mt-8">
+    <h2 className="text-lg font-semibold mt-4 flex items-center gap-2">
+      <Crown size={20} className='text-blue-600'/>
+      Subscription Status
+    </h2>
+    <div className="p-6 rounded-lg bg-linear-to-br from-blue-50 to-purple-50 dark:from-blue-900/20 to-purple-950/20">
+    {
+      !user.subscription ? (<>
+      <div className="flex items-center justify-between flex-wrap gap-4">
+<div>
+  <p className="font-semibold text-lg mb-1">
+    No Active Subscription
+  </p>
+  <p className='text-sm opacity-70 '>
+    Subscribe to unlock premium features and benefits.
+  </p>
+</div>
+
+<Button className="gap-2" onClick={() => router.push('/subscribe')}>
+  <Crown size={18}/>
+  Subscribe Now
+</Button>
+
+      </div>
+      </>) : 
+
+      new Date(user.subscription).getTime()>Date.now() ? <div className="flex items-center justify-between flex-wrap gap-4">
+<div>
+  <div className="flex items-center gap-2 mb-2">
+    <CheckCircle2 size={20} className='text-green-600'/>
+    <p className="font-semibold text-lg text-green-600">
+      Active Subscription
+    </p>
+  </div>
+
+<p className="text-sm opacity-70">
+  Valid until : {""}
+
+  {
+    new Date(user.subscription).toLocaleDateString(
+      "en-US",{
+        year : "numeric",
+        month: "long",
+        day : "numeric",
+      }
+    )
+  }
+</p>
+
+</div>
+
+
+<div className="flex items-center gap-2 px-4 py-2 rounded-full bg-green-700 text-white font-medium">
+  <CheckCircle2 size={18}/>
+  Subscribed
+</div>
+
+
+      </div> : (<>
+      <div className="flex items-center justify-between flex-wrap gap-4">
+        <div>
+          <div className="flex items-center gap-2 mb-2">
+            <AlertTriangle size={20} className='text-red-600'/>
+            <p className="font-semibold text-lg text-red-600 ">
+              Subscription Expired
+            </p>
+          </div>
+
+
+<p className="text-sm opacity-70">
+  Expired On : {""} 
+  {
+    new Date(user.subscription).toLocaleDateString(
+      "en-US",{
+        year : "numeric",
+        month: "long",
+        day : "numeric",
+      }
+    )
+  }
+</p>
+        </div>
+
+
+<Button variant={"destructive"} className="gap-2" onClick={() => router.push("/subscribe")}>
+  <RefreshCcw size={18}/>
+  Renew Subscription
+</Button>
+
+
+      </div>
+      </>)
+    }
+    </div>
+  </div>
+  }
+  </>
+}
+
         </div>
       </Card>
 
-{/** */}
+{/**Dialog box for edit */}
 <Dialog>
   <DialogTrigger asChild>
     <Button ref={editRef} variant={"outline"}
